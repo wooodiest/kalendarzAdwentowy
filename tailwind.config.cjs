0a445f9a3d85bb659f7d1025a0ede241ae01f3ea
wooodiest <michalkuchnickiisc@gmail.com>
2025-11-24 23:06:16 +0100

    Added initial project files

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        winter: {
+          blue: '#4A90E2',
+          light: '#E8F4F8',
+          dark: '#2C5282',
+        },
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
+
