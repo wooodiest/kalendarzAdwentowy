fd5cca515a103ea53bdd5528aa91adea0bf5d48d
wooodiest <michalkuchnickiisc@gmail.com>
2025-11-28 18:47:47 +0100

    dodano vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Jeśli wdrażasz na głównej domenie, użyj '/'
-  // Jeśli wdrażasz w podfolderze, użyj '/nazwa-folderu/'
   base: process.env.VITE_BASE_PATH || '/',
+  server: {
+    historyApiFallback: true
+  }
 })
