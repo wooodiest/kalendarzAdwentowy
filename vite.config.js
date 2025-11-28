17af272b4419400afde5435218b9f3185e7dcaf3
wooodiest <michalkuchnickiisc@gmail.com>
2025-11-28 18:23:00 +0100

    dwqdqd

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/data/**/*',
-          dest: 'data'
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   base: process.env.VITE_BASE_PATH || '/kalendarzAdwentowy',
 })
